09822f66263878d3c6ad363316b78cb2448030aa
taka-gift <62645352+taka-gift@users.noreply.github.com>
2020-04-03 20:46:08 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -26,8 +26,8 @@ NetworkType = log.ThermalData.NetworkType
 NetworkStrength = log.ThermalData.NetworkStrength
 CURRENT_TAU = 15.   # 15s time constant
 CPU_TEMP_TAU = 5.   # 5s time constant
-DAYS_NO_CONNECTIVITY_MAX = 7  # do not allow to engage after a week without internet
-DAYS_NO_CONNECTIVITY_PROMPT = 4  # send an offroad prompt after 4 days with no internet
+DAYS_NO_CONNECTIVITY_MAX = 997  # do not allow to engage after 997 days without internet
+DAYS_NO_CONNECTIVITY_PROMPT = 994  # send an offroad prompt after 994 days with no internet
 
 LEON = False
 last_eon_fan_val = None
